7f24a2d9b444e7b7398a7d5386fa980027061002
Devadathan <devadathanappu69@gmail.com>
2025-08-12 16:17:00 +0530

    new commit.or last one gonna try next one

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+app.use(cors());
+
 
 const authRoutes = require('./routes/auth');
 const eventRoutes = require('./routes/events');
